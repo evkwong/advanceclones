ad477a77ed692c2a4147c10be56fee55553e3549
Evan Wong <evan.wong00@gmail.com>
2017-04-22 14:39:53 -0700

    Button presses can change url location

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.jade', { title: 'Express' });
 });
 
-router.get('/registration', function(req, res) {
-  res.render('registration', {
-    title: 'Registration'
-  });
+router.get('/registration', function(req, res, next) {
+  res.render('registration.jade', { title: 'Registration'});
 });
 
 module.exports = router;
