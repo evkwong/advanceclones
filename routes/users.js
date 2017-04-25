673bbea69165907b192c9768d70ec3c18cee63f8
gojirra <gojirra@gmail.com>
2017-04-25 15:47:44 -0700

    Testing /register.

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) { //Is next needed here?
   res.send('respond with a resource');
 });
 
 router.post('/register', function(req, res){
 	//Do something.
-	console.log('Post request to /users')
+	console.log('Post request to /users');
+	console.log(req.method, req.url);
 });
 
 router.get('/login', function(req, res){
 	//Do something.
-})
+});
 
 module.exports = router;
