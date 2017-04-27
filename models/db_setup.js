c40401d3ca1f1734ddfc9a65d2f4345e01c3f770
gojirra55 <gojirra@gmail.com>
2017-04-27 10:13:12 -0700

    Implementing models.

---
+++ models/db_setup.js
@@ -0,0 +1,21 @@
+var pgp = require('pg-promise')();
+//const pg = require('pg');
+var fs = require('fs'); //File sync.
+var sql = fs.readFileSync('../models/db_schema.sql').toString();
+var testString = 'postgres://vampireSnakes:blehssss@localhost:5432/advanceclones';
+
+const connectionString = process.env.DATABASE_URL || testString;
+var db = pgp(connectionString);
+db.connect();
+db.query(sql);
+db.close();
+
+/*exports.connectDB = function(input) {
+	db.connect(
+)};*/
+
+module.exports = db; //Export reference to database.
+
+//const client = new pgp.Client(connectionString);
+//const query = db.query(sql);
+//query.on('end', () => { client.end(); });
