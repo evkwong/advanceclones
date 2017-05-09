c395a72e3f8c3b78bc9411eee9ad8e357080db15
Josh Nagel <nageljp27@gmail.com>
2017-05-09 14:38:32 -0700

    added create game route, and added flash messages conditional

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,15 @@ router.get('/lobby', function(req,res, next) {
   res.render('lobby.jade', { title: 'Lobby'});
 });
 
+//Create Game Page
+router.get('/createGame', function(req,res,next){
+	res.render('createGame.jade', {title: 'Create Game'});
+});
+
+//Flash messages
+if(error.length > 0){
+	req.flash('error', error);
+	res.redirect('/', {message:req.flash('error')});
+}
+
 module.exports = router;
