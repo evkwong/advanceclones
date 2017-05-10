32fc394f41164e87fc053820fdd090c4bcdd9492
Evan Wong <evan.wong00@gmail.com>
2017-05-10 14:46:34 -0700

    got javascript to draw image on canvas

--- public/game/test.js
+++ public/game/test.js
@@ -1,29 +1,6 @@
-const gameDraw = {
-		canvas: document.getElementById("gameDraw"),
-		context: null
-}
-
-$('#gameDraw').ready(() => {
-		gameDraw.canvas.width = gameDraw.width;
-		gameDraw.canvas.height = gameDraw.height;
-		gameDraw.context = gameDraw.canvas.getContext("2d");
-
-		})
-});
-
-var socket = (app, server) => {
-		var io = socketIO(server);
-		app.set('io', io);
-
-		io.connect('connection', function(data) {
-			console.log("Connected: ", data);	
-		})
-}
-
-gameDraw.onload = function() {
-		gameDraw.drawImage(background, 0, 0);
-		gameDraw.fillStyle = "rbga(200, 0, 0, 0.5)";
-		gameDraw.fillRect(0, 0, 500, 500);
+window.onload = function() {
+		var canvas = document.getElementById("gameDraw");
+		var ctx = canvas.getContext("2d");
+		var image = document.getElementById("background");
+		ctx.drawImage(image, 10, 10);
 };
-
-
