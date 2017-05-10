d18d7d5d008fb64b7ebbb593421f8c4352ef0aec
Eldres <Eldres@users.noreply.github.com>
2017-05-10 14:17:59 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var error;
-
 
 //Landing Page
 router.get('/', function(req, res, next) {
@@ -22,11 +20,5 @@ router.get('/lobby', function(req,res, next) {
 router.get('/createGame', function(req,res,next){
 	res.render('createGame.jade', {title: 'Create Game'});
 });
-
-//Flash Messages
-//if(error.length > 0){
-//	req.flash('error_messages', error);
-//	res.redirect('/');
-//}
 
 module.exports = router;
