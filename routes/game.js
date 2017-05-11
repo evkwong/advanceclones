e3c1d228916079f1becc23fe3458364cd26ba1d5
Evan Wong <evan.wong00@gmail.com>
2017-05-11 15:06:23 -0700

    committing before merging

--- routes/game.js
+++ routes/game.js
@@ -57,7 +57,7 @@ router.post('/new_game', function(req, res) {
 		//Send variables to game js.
 		//res.send(path.basename('/images/map_' + map + '.png'));
 		var mapPath = path.basename('/images/map_' + mapID + '.png');
-		res.render('testGame', {title: title, mapPath: mapPath, mapID: map});
+		res.render('testGame', {title: title, mapPath: mapPath, gameID: gameID});
 
 	}
 });
